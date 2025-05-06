9881a61b53fc8f5b447b07d0953c66e2f1e41bb2
jthorne2000 <jatjessto@gmail.com>
2025-05-06 20:50:51 +0000

    removed print, print(len), and print commands from extractKeywords.py

--- extractKeywords.py
+++ extractKeywords.py
@@ -3,12 +3,7 @@ f = open('gazetteer.txt','r')
 allKeywords = f.read().lower().split("\n")
 f.close()
 
-print(allKeywords)
-print(len(allKeywords))
-
 #Import the texts you want to search
 f = open('text.txt','r')
 allTexts = f.read().lower().split("\n")
 f.close()
-
-print(allTexts)
