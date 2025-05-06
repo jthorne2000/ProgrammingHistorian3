17a07bdb788e1be37458d5772af87c32eb4a82c7
jthorne2000 <jatjessto@gmail.com>
2025-05-06 20:56:09 +0000

    removing punctuation from allTexts

--- extractKeywords.py
+++ extractKeywords.py
@@ -7,3 +7,13 @@ f.close()
 f = open('text.txt','r')
 allTexts = f.read().lower().split("\n")
 f.close()
+
+for entry in allTexts:
+    #for each entry:
+    allWords = entry.split(' ')
+    for words in allWords:
+
+        #remove punctuation that will interfere with matching
+        words = words.replace(',','')
+        words = words.replace('.','')
+        words = words.replace(';','')
